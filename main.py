2e62fb45fc7a6680a4139b7c9f0028be0c5e0cfb
Guillaume Verger <verger.guillaume@gmail.com>
2025-11-08 18:06:45 +0000

    2nd

--- main.py
+++ main.py
@@ -1,26 +1,127 @@
 from pathlib import Path
+from typing import Any
 import rdflib as rdf
 from datetime import datetime
+import networkx as nx
 
-def get(graph: rdf.Graph, prop_query: str, identifier: str, indent = ""):
-    # query = query.replace("$ID", 'F20171002T0930Z_BE_EQ_2.xml:_17086487-56ba-4979-b8de-064025a6b4da')
-    query = prop_query.replace("$ID", identifier)
+FILE_NS = "NSFILE_"
 
-    # print(query)
+class CGMESNode():
+    def __init__(self, id: str):
+        self.id = id
+        self.props = {}
+        self.children = {}
+
+    def add_value(self, key, value):
+        self.props[key] = value
+
+    def add_child(self, filiation, child):
+        self.children[filiation] = child
+
+    def __repr__(self) -> str:
+        rep = f"{self.id}:\n"
+        if len(self.props) > 1:
+            rep += "Properties:\n"
+            for key in sorted(self.props.keys()):
+                rep += f"  {key}: {self.props[key]}\n"
+        if len(self.props) > 1:
+            rep += "Children:\n"
+            for key in sorted(self.children.keys()):
+                rep += f"  {key}: {self.children[key]}\n"
+
+        return rep
+
+
+def properties(graph: rdf.Graph, identifier: str) -> CGMESNode:
+    query = """
+SELECT ?s ?p ?o
+WHERE {
+  VALUES ?s { $ID }
+?s ?p ?o.
+}
+LIMIT 1000
+        """
+
+    query = query.replace("$ID", identifier)
+
+    node = CGMESNode(identifier)
+
+    for res in graph.query(query):
+        raw_p = res.get("p")
+        p = raw_p.n3(graph.namespace_manager)
+        raw_o = res.get("o")
+        o = raw_o.n3(graph.namespace_manager)
+
+        if p == "rdf:type":
+            node.add_value(p, o)
+        elif isinstance(raw_o, rdf.Literal):
+            node.add_value(p, raw_o.value)
+        elif isinstance(raw_o, rdf.URIRef):
+            node.add_child(p, o)
+
+    return node
+
+def ascendants(graph: rdf.Graph, identifier: str, seen:list[str]=[]) -> list[str]:
+    if identifier in seen:
+        return []
+
+    seen.append(identifier)
+
+    query = """
+SELECT ?s ?p ?o
+WHERE {
+?o ?p $ID.
+  VALUES ?s { $ID }
+}
+LIMIT 10000
+        """
+    query = query.replace("$ID", identifier)
 
     references = []
 
     for res in graph.query(query):
-        s = res.get("s")
-        p = res.get("p")
-        if isinstance(p, rdf.URIRef):
-            p.n3(graph.namespace_manager)
         o = res.get("o")
+        n3 = o.n3(graph.namespace_manager)
 
-        print(f"{indent}{s.n3(graph.namespace_manager)} {p.n3(graph.namespace_manager)} {o.n3(graph.namespace_manager)}")
+        if n3.startswith(FILE_NS) and isinstance(o, rdf.URIRef):
+            identifier = o.n3(graph.namespace_manager)
+            if identifier.startswith("F"):
+                pass
+            references.append(o.n3(graph.namespace_manager))
+            references.extend(ascendants(graph, o.n3(graph.namespace_manager), seen))
 
-        if isinstance(o, rdf.URIRef):
-            get(graph, prop_query, o.n3(graph.namespace_manager), f"  {indent}")
+    return references
+
+def descendants(graph: rdf.Graph, identifier: str, seen: list[str]=[]) -> list[str]:
+    if identifier in seen:
+        return []
+    seen.append( identifier)
+
+    query = """
+SELECT ?s ?p ?o
+WHERE {
+  VALUES ?s { $ID }
+?s ?p ?o.
+}
+LIMIT 1000
+        """
+    query = query.replace("$ID", identifier)
+
+    references = []
+
+    for res in graph.query(query):
+        o = res.get("o")
+        n3 = o.n3(graph.namespace_manager)
+
+
+        if n3.startswith(FILE_NS) and isinstance(o, rdf.URIRef):
+            identifier = o.n3(graph.namespace_manager)
+            if identifier.startswith("F"):
+                pass
+            references.append(o.n3(graph.namespace_manager))
+            references.extend(descendants(graph, o.n3(graph.namespace_manager), seen))
+
+    return references
 
 def main():
 
@@ -37,7 +138,7 @@ def main():
     for f in small_grid_folder.glob("*.xml"):
         print(f)
         graph.parse(f)
-        graph.bind("F"+f.name, f"file://{f.absolute()}#")
+        graph.bind(FILE_NS+f.name, f"file://{f.absolute()}#")
 
     stop = datetime.now()
     print(f"loading duration: {stop - start}")
@@ -49,12 +150,26 @@ def main():
     with open("./query_up.rq") as qf:
         asc_query = qf.read()
 
-    # get(graph, prop_query, 'F20171002T0930Z_BE_EQ_2.xml:_17086487-56ba-4979-b8de-064025a6b4da')
-    get(graph, asc_query, 'F20171002T0930Z_BE_EQ_2.xml:_17086487-56ba-4979-b8de-064025a6b4da')
+    identifier = FILE_NS + '20171002T0930Z_BE_EQ_2.xml:_17086487-56ba-4979-b8de-064025a6b4da'
+    # identifier = FILE_NS + 'CGMES_v2.4.15_RealGridTestConfiguration_EQ_V2.xml:_426798065_ACLS'
+    # print(properties(graph, identifier))
+    all = [identifier] + descendants(graph, identifier) + ascendants(graph, identifier)
+    # print(descendants(graph, identifier))
+    # print(ascendants(graph, identifier))
 
-    # get(graph, prop_query, 'FCGMES_v2.4.15_RealGridTestConfiguration_EQ_V2.xml:_426798065_ACLS')
-    # get(graph, asc_query, 'FCGMES_v2.4.15_RealGridTestConfiguration_EQ_V2.xml:_426798065_ACLS')
+    g = nx.DiGraph()
 
+    g.add_nodes_from(all)
+
+    for identifier in all:
+        n = properties(graph, identifier)
+        for p in n.props:
+            g.nodes[identifier][p] = n.props[p]
+
+        for c in n.children:
+            g.add_edge(identifier, n.children[c], rel=c)
+
+    nx.draw_networkx(g)
 
 
 if __name__ == "__main__":
