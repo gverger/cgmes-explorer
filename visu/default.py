ba187ff6b8eca8ac203a1c31dddbb1ae16bcb07a
Guillaume Verger <verger.guillaume@gmail.com>
2025-11-15 17:01:31 +0100

    remove daq

--- visu/default.py
+++ visu/default.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import dash
 import dash_cytoscape as cyto
 import dash_bootstrap_components as dbc
-import dash_daq as daq
 from dash import ALL, Input, Output, State, dcc, html
 from loguru import logger
 
